83422e4f733c5c0b036b7e414edcf664e3815c2f
joaoprcf <joaoprcf@ua.pt>
2022-05-17 13:27:01 +0100

    header file updated

--- src/setrp3.h
+++ src/setrp3.h
@@ -12,10 +12,49 @@
 #define RETURNCREDIT 4
 
 /* Function prototypes */
+
+/**
+ * @brief Insert coin logic, the credit is increased according to the value of the inserted coin.
+ *
+ * @param[in] index Coin index to be inserted
+ * @return int
+ */
 int insertCoin(int index);
+
+/**
+ * @brief Buy selected product to be dispensed if enough credit is available.
+ *
+ * @return int
+ */
 int select(void);
+
+/**
+ * @brief Return credit to user, setting the current device value to 0.
+ *
+ * @return int
+ */
+int returnCredit(void);
+
+/**
+ * @brief Change the current selected product
+ *
+ * @param[in] direction Either -1 or 1 to select the previous or the next product respectively.
+ * @return int
+ */
 int changeSelectedProduct(int);
+
+/**
+ * @brief Reset initial state.
+ *
+ */
 void resetInitialState(void);
+
+/**
+ * @brief Manage events and define corresponding actions to the state machine.
+ *
+ * @param[in] event Event triggered
+ * @param[in] index Event external data
+ */
 void eventLoop(char event, int index);
 
 #endif
